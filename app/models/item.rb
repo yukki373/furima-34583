3b9383e06cef514b50f3d34f109cf505067d8f8c
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-03-14 17:39:40 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+
+  validates :name,                presence: true
+  validates :description,         presence: true
+  validates :category_id,         presence: true
+  validates :status_id,           presence: true
+  validates :shipping_charge_id,  presence: true
+  validates :shipping_area_id,    presence: true
+  validates :days_to_ship_id,     presence: true
+  validates :price,               presence: true
+
   belongs_to :user
   has_one_attached :image
 end
