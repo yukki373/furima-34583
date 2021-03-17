cb2fca8b7d9cb8ae882bcf4ca95b92bfa9af053d
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-03-17 16:45:02 +0900

    修正５

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :set_action, except: [:index, :new]
 
   def index
     @items = Item.order("created_at DESC")
@@ -11,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -20,21 +20,19 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
     end
   end
+  
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
@@ -43,5 +41,9 @@ class ItemsController < ApplicationController
   def contributor_confirmation
     @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user.id
+  end
+
+  def set_action
+    @item = Item.find(params[:id])
   end
 end
