614bc801b899898f0255f3fb94ccf3bc46df5052
yukki373 <78712648+yukki373@users.noreply.github.com>
2021-03-19 14:35:07 +0900

    修正３

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :order_address do
     postal_code { '123-4567' }
-    shipping_area_id { 2 }
-    municipality { 1 }
+    shipping_area_id { "2" }
+    municipality { "1" }
     address { '1-1' }
     building_name { '東京団地'}
     phone_number { '00000000000' }
